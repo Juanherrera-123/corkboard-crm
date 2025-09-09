1ea6699b5301f830924e770109bc4bb1a90f6126
Juanherrera-123 <jherreran@unal.edu.co>
2025-09-09 11:28:15 -0500

    feat: add layout dimension constants

---
+++ lib/layout.ts
@@ -0,0 +1,9 @@
+export const gridCols = 10;
+
+export const DEFAULT_W = 4;
+export const DEFAULT_H = 3;
+
+export const minW = 2;
+export const maxW = gridCols;
+export const minH = 2;
+export const maxH = 8;
